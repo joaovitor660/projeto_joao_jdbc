f7789fba68f57ee3650d42788309b65842931030
joaovitor660 <125267487+joaovitor660@users.noreply.github.com>
2024-05-16 12:17:31 -0300

    Add files via upload

---
+++ caixa_castelo/target/conexao/conexao.java
@@ -0,0 +1,45 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexao {
+
+    private String url = "jdbc:mysql://127.0.0.1:3306/loja";
+    private String user = "root";
+    private String password = "";
+ 
+    private Connection conn;
+
+
+    public Connection getConexao() {
+
+        try {
+
+
+
+
+
+
+
+
+
+
+            
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
